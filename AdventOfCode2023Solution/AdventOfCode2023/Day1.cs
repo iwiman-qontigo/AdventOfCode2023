b6c48c77add0c15af8e5a17134cf63279071ccb4
Ignacio Wiman <iwiman@qontigo.com>
2023-12-12 10:06:17 -0300

    Start Day1 part 1

--- AdventOfCode2023Solution/AdventOfCode2023/Day1.cs
+++ AdventOfCode2023Solution/AdventOfCode2023/Day1.cs
@@ -2,6 +2,68 @@
 {
     public class Day1
     {
+        public int GetSumOfNumbers()
+        {
+            var input = GetInput();
 
+            var sum = 0;
+            using (var reader = new StringReader(input))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var number = GetNumber(line);
+                    sum += number;
+                }
+            }
+
+            return sum;
+        }
+
+        private int GetNumber(string rawLine)
+        {
+            var line = rawLine.Trim();
+
+            var firstDigit = GetFirstDigit(line);
+            
+            var lastDigit = GetLastDigit(line);
+
+            var fullNumber = $"{firstDigit}{lastDigit}";
+
+            return int.Parse(fullNumber);
+        }
+
+        private char GetFirstDigit(string line)
+        {
+            foreach (var character in line)
+            {
+                if (int.TryParse(character.ToString(), out _))
+                {
+                    return character;
+                }
+            }
+
+            throw new Exception("No numeric character in the line provided.");
+        }
+
+        private char GetLastDigit(string line)
+        {
+            foreach (var character in line.Reverse())
+            {
+                if (int.TryParse(character.ToString(), out _))
+                {
+                    return character;
+                }
+            }
+
+            throw new Exception("No numeric character in the line provided.");
+        }
+
+        private string GetInput()
+        {
+            return @"two934seven1
+                8825eightknfv
+                sevenoneqbfzntsix55";
+        }
     }
 }
--- AdventOfCode2023Solution/AdventOfCode2023Tests/Day1Tests.cs
+++ AdventOfCode2023Solution/AdventOfCode2023Tests/Day1Tests.cs
@@ -1,9 +1,15 @@
+using AdventOfCode2023;
 using NUnit.Framework;
 
 namespace AdventOfCode2023Tests
 {
     public class Day1Tests
     {
-        
+        [Test]
+        public void Day1Test()
+        {
+            var actual = new Day1().GetSumOfNumbers();
+            Assert.That(actual == 230, $"Result = {actual}");
+        }
     }
 }
